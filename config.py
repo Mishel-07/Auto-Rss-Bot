2ef4bb4f92840193690d7378a14e23b8f239d003
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-01-12 14:26:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import environ
+
+API_ID = int(environ.get('API_ID', '283839'))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+SEND_CHANNEL = int(environ.get('SEND_CHANNEL', ''))
