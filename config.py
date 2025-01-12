c7c23db3e94a14282371a6152fbc77ef3413ba64
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-01-12 15:41:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 from os import environ
 
+def chk_true_or_false(m):
+  if m == 'True':
+    return True  
+  else:
+    return False
+
+TORRENT = chk_true_or_false(environ.get('TORRENT', ''))
+
+
 API_ID = int(environ.get('API_ID', '283839'))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
