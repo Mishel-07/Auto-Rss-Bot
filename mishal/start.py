6dde6d0ec3de9e2654d010f80f4fb5b90235d651
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-01-12 13:59:46 +0530

    Create start.py

---
+++ mishal/start.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["start"]))
+async def start(client, message):        
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Source", url="https://github.com/Mishel-07/Auto-Rss-Bot")], [InlineKeyboardButton("Support Group", url="https://t.me/XBOTSUPPORTS")]])
+    await message.reply_text(f"Hey {message.from_user.mention}, I'm an auto RSS bot. You can find my source code by clicking the button below.", reply_markup=reply_markup)
